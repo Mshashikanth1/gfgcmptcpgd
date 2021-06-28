c4091eeab75a4b2bf97e194af83874434a1093ad
shashikanth <shashikanthvarma98@gmail.com>
2021-06-28 10:55:17 +0000

    Update reverse_squared_sum.py

--- reverse_squared_sum.py
+++ reverse_squared_sum.py
@@ -1,12 +1,23 @@
 #code
 def reverse_rec(a,l,h):
-    if l<h:
+    while l<h:
         a[l],a[h]=a[h],a[l]
-        reverse_rec(a,l+1,h-1)
+        l+=1
+        h-=1
+        
 for _ in range(int(input())):
     n=int(input())
     a=[]
+    sig=1
     for i in map(int,input().split()):
         a.append(i*i)
     reverse_rec(a,0,n-1)
-    print(a)
+    sum1=0
+    for i in a:
+        if sig==1:
+          sum1+=i
+          sig=0
+        else:
+            sum1-=i
+            sig=1
+    print(sum1)
